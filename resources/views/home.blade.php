8830cf5d3b8cbd9207964e496a9cc1619c55656d
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-09-20 17:13:05 -0500

    Creating a form with random amounts using Bootstrap in Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,28 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Make a payment</div>
 
                 <div class="card-body">
-                    You are logged in!
+                    <form action="#" method="POST" id="paymentForm">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label>How much you want to pay?</label>
+                                <input
+                                    type="number"
+                                    min="5"
+                                    step="0.01"
+                                    class="form-control"
+                                    name="value"
+                                    value="{{ mt_rand(500, 100000) / 100 }}"
+                                >
+                            </div>
+                        </div>
+                        <div class="text-center mt-3">
+                            <button type="submit" id="payButton" class="btn btn-primary btn-lg">Pay</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
