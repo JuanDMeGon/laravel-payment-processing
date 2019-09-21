60678a8da77973464511e0e44d5ad760f28e88ea
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-09-21 01:24:32 -0500

    Controlling from Laravel the capture of a payment with PayPal

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -39,7 +39,9 @@ class PaymentController extends Controller
 
     public function approval()
     {
-        //
+        $paymentPlatform = resolve(PayPalService::class);
+
+        return $paymentPlatform->handleApproval();
     }
 
     public function cancelled()
