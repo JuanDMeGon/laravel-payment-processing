d14697e6f0792f7ba769945581442937920d6e6b
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-02 17:31:49 -0500

    Handling with Laravel the creation of a payment intent in Stripe

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -34,8 +34,6 @@ class PaymentController extends Controller
             'payment_platform' => ['required', 'exists:payment_platforms,id'],
         ];
 
-        dd($request->all());
-
         $request->validate($rules);
 
         $paymentPlatform = $this->paymentPlatformResolver
