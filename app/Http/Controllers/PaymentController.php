8360ff1296e3ae3ebb1f297f86657ba9adc9ea66
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-02 17:00:22 -0500

    Obtaining a token that represents the payment method in Stripe

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -34,6 +34,8 @@ class PaymentController extends Controller
             'payment_platform' => ['required', 'exists:payment_platforms,id'],
         ];
 
+        dd($request->all());
+
         $request->validate($rules);
 
         $paymentPlatform = $this->paymentPlatformResolver
--- resources/views/components/stripe-collapse.blade.php
+++ resources/views/components/stripe-collapse.blade.php
@@ -37,6 +37,8 @@
 
 <small class="form-text text-muted" id="cardErrors" role="alert"></small>
 
+<input type="hidden" name="payment_method" id="paymentMethod">
+
 @push('scripts')
 <script src="https://js.stripe.com/v3/"></script>
 <script>
@@ -46,5 +48,36 @@
     const cardElement = elements.create('card');
 
     cardElement.mount('#cardElement');
+</script>
+
+<script>
+    const form = document.getElementById('paymentForm');
+    const payButton = document.getElementById('payButton');
+
+    payButton.addEventListener('click', async(e) => {
+        if (form.elements.payment_platform.value === "{{ $paymentPlatform->id }}") {
+            e.preventDefault();
+
+            const { paymentMethod, error } = await stripe.createPaymentMethod(
+                'card', cardElement, {
+                    billing_details: {
+                        "name": "{{ auth()->user()->name }}",
+                        "email": "{{ auth()->user()->email }}"
+                    }
+                }
+            );
+
+            if (error) {
+                const displayError = document.getElementById('cardErrors');
+
+                displayError.textContent = error.message;
+            } else {
+                const tokenInput = document.getElementById('paymentMethod');
+
+                tokenInput.value = paymentMethod.id;
+                form.submit();
+            }
+        }
+    });
 </script>
 @endpush
