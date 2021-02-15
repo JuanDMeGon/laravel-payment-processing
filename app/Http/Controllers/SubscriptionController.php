d657ad302786b1591159ec864ea72f47e884712b
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2021-02-15 18:59:56 -0500

    Start the Payment of a Subscription With a Payment Platform

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,18 +4,23 @@ namespace App\Http\Controllers;
 
 use App\PaymentPlatform;
 use App\Plan;
+use App\Resolvers\PaymentPlatformResolver;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    public function __construct()
+    protected $paymentPlatformResolver;
+
+    public function __construct(PaymentPlatformResolver $paymentPlatformResolver)
     {
         $this->middleware('auth');
+
+        $this->paymentPlatformResolver = $paymentPlatformResolver;
     }
 
     public function show()
     {
-        $paymentPlatforms = PaymentPlatform::/*where('subscriptions_enabled', true)->*/get();
+        $paymentPlatforms = PaymentPlatform::where('subscriptions_enabled', true)->get();
 
         return view('subscribe')->with([
             'plans' => Plan::all(),
@@ -23,9 +28,21 @@ class SubscriptionController extends Controller
         ]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $rules = [
+            'plan' => ['required', 'exists:plans,slug'],
+            'payment_platform' => ['required', 'exists:payment_platforms,id'],
+        ];
 
+        $request->validate($rules);
+
+        $paymentPlatform = $this->paymentPlatformResolver
+            ->resolveService($request->payment_platform);
+
+        session()->put('subscriptionPlatformId', $request->payment_platform);
+
+        return $paymentPlatform->handleSubscription($request);
     }
 
     public function approval()
