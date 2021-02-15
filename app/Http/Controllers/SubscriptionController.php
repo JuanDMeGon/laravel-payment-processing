44a72678a18d6f3da7ac4c6fb9b84da4257ae609
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2021-02-15 18:21:11 -0500

    Implementing the Actions To Start the Subscription of a User

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    //
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function approval()
+    {
+
+    }
+
+    public function cancelled()
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,22 @@ Route::post('/payments/pay', 'PaymentController@pay')->name('pay');
 Route::get('/payments/approval', 'PaymentController@approval')->name('approval');
 Route::get('/payments/cancelled', 'PaymentController@cancelled')->name('cancelled');
 
+Route::prefix('subscribe')
+    ->name('subscribe.')
+    ->group(function () {
+        Route::get('/', 'SubscriptionController@show')
+            ->name('show');
+
+        Route::post('/', 'SubscriptionController@store')
+            ->name('store');
+
+        Route::get('/approval', 'SubscriptionController@approval')
+            ->name('approval');
+
+        Route::get('/cancelled', 'SubscriptionController@cancelled')
+            ->name('cancelled');
+    });
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
